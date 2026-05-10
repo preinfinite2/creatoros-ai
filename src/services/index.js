b1da678faba312e116d7cd85759b5ecf3ce71faa
preinfinite2 <preciousoluwalola2@gmail.com>
2026-05-10 02:30:36 +0100

    index.js

---
+++ src/services/index.js
@@ -0,0 +1,5 @@
+// src/services/index.js (Services barrel export)
+export { authService } from './authService'
+export { projectService } from './projectService'
+export { generationService } from './generationService'
+export { apiService } from './apiService'
