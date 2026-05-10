903da62ad7c6efad401ec594cb675749e4d93109
preinfinite2 <preciousoluwalola2@gmail.com>
2026-05-10 01:09:41 +0100

    index.js

---
+++ src/ai/index.js
@@ -0,0 +1,5 @@
+// src/ai/index.js
+export { titleEngine } from './engines/titleEngine'
+export { hookEngine } from './engines/hookEngine'
+export { scriptEngine } from './engines/scriptEngine'
+export { ideaEngine } from './engines/ideaEngine'
