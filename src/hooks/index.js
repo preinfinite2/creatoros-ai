884a54c5f9f74b54505946e8caebde6acb418adf
preinfinite2 <preciousoluwalola2@gmail.com>
2026-05-10 02:24:57 +0100

    index.js

---
+++ src/hooks/index.js
@@ -0,0 +1,8 @@
+// src/hooks/index.js (Hooks barrel export)
+export { useAuth } from './useAuth'
+export { useGeneration } from './useGeneration'
+export { useProjects } from './useProjects'
+export { useClipboard } from './useClipboard'
+export { useLocalStorage } from './useLocalStorage'
+export { useMediaQuery, useIsMobile, useIsTablet, useIsDesktop } from './useMediaQuery'
+export { useKeyboardShortcut, SHORTCUTS } from './useKeyboardShortcut'
